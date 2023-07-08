a42493ef1ef59a0bcb946040a7703852108e307e
jake-knox- <jakeknox11@hotmail.co.uk>
2023-07-08 13:52:50 +0100

    skeleton of new project, removed old react template

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+// import ways to navigate
+import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+// import styles + react logo (not needed)
+import logo from '../logo.svg';
+import '../styles/App.css';
+
+// import the other pages
+import LoginPage from './LoginPage';
+import BookshelfPage from './BookshelfPage';
+import CRUDPage from './CRUDPage';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>test</h1>
+        
+    </div>
+  );
+}
+
+export default App;
